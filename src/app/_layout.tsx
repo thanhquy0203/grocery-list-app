abd882083de203f80d6de9841caf835fa0e225d8
thanhquy0203 <22638141.quy@student.iuh.edu.vn>
2025-11-15 09:51:12 +0700

    feat(Q2): create grocery_items table and seed sample

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,17 @@
+import { SQLiteProvider } from "expo-sqlite";
 import "../global.css";
-import { Slot } from "expo-router";
+import { Slot, Stack } from "expo-router";
+import { initDb } from "@/db/db";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function Layout() {
-  return <Slot />;
+  return (
+    <SQLiteProvider databaseName="grocery.db" onInit={(db) => initDb(db)}>
+       <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1 }}>
+          <Stack screenOptions={{ headerShown: false }} />
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </SQLiteProvider>
+  );
 }
--- src/db/db.ts
+++ src/db/db.ts
@@ -11,5 +11,35 @@ export const initDb = async (db: SQLiteDatabase) => {
       bought INTEGER DEFAULT 0,
       created_at INTEGER
     );
-  `);
+   `);
+
+  
+ type CountRow = { count: number };
+
+const rows = await db.getAllAsync<CountRow>(
+  `SELECT COUNT(*) as count FROM grocery_items`
+);
+
+const isEmpty = rows[0]?.count === 0;
+
+  
+  if (isEmpty) {
+    await db.runAsync(
+      `INSERT INTO grocery_items (name, quantity, category, bought, created_at)
+       VALUES (?, ?, ?, ?, ?)`,
+      ["Sữa", 1, "Đồ uống", 0, Date.now()]
+    );
+
+    await db.runAsync(
+      `INSERT INTO grocery_items (name, quantity, category, bought, created_at)
+       VALUES (?, ?, ?, ?, ?)`,
+      ["Trứng", 12, "Thực phẩm", 0, Date.now()]
+    );
+
+    await db.runAsync(
+      `INSERT INTO grocery_items (name, quantity, category, bought, created_at)
+       VALUES (?, ?, ?, ?, ?)`,
+      ["Bánh mì", 2, "Đồ ăn sáng", 0, Date.now()]
+    );
+  }
 };
